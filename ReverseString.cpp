0df11ab6aca459021a8349888a0b09eae7f435e3
rythem253 <miolavoro9@gmail.com>
2025-06-15 18:40:15 -0600

    Create ReverseString.cpp
    Reverse a string mini project

---
+++ ReverseString.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString() {
+        string s;
+        
+        cout << "Enter a word to reverse: ";
+        cin >> s;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            cout << s[i];
+        }
+        cout << endl;  // just to add a newline after output
+    }
+};
+
+int main() {
+    Solution sol;
+    sol.reverseString();
+    return 0;
+}
